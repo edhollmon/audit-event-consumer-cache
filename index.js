53cae487d9c91fc708a02052d0579b9b492354d1
Edward Hollmon <edhollmon@gmail.com>
2025-09-23 04:00:26 -0700

    Added redis

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 import { Kafka } from 'kafkajs';
+import { createClient } from 'redis';
 import 'dotenv/config';
 
 const clientId = 'audit-event-consumer-cache'
 const brokers = ['localhost:9092']
 const groupId = 'audit-event-cache';
 const topic = 'audit-event-topic';
+const cacheKeyPrefix = 'events:tenant:'
+
+// Setup Redis
+const client = createClient();
+client.on('error', err => console.log('Redis Client Error', err));
+await client.connect();
 
 // Kafka Setup
 const kafka = new Kafka({
@@ -16,8 +23,10 @@ const consumer = kafka.consumer({
     groupId
 })
 
+
 await consumer.connect()
 await consumer.subscribe({ topic, fromBeginning: true })
+
 
 await consumer.run({
   eachMessage: async ({ topic, partition, message }) => {
@@ -27,6 +36,6 @@ await consumer.run({
     console.log(event)
 
     // Send to cache
-    
+    client.rPush(`${cacheKeyPrefix}${event?.tenantId}`, JSON.stringify(event));
   },
 })
